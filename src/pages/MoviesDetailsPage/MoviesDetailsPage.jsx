96a89cc03a558e24e6b120be19f4d2a91dab1b0b
AndreyMad <andrey.mad.87@gmail.com>
2020-02-14 00:00:00 +0200

    fix bugs

--- src/pages/MoviesDetailsPage/MoviesDetailsPage.jsx
+++ src/pages/MoviesDetailsPage/MoviesDetailsPage.jsx
@@ -13,7 +13,11 @@ class MoviesDetailsPage extends Component {
   };
 
   state = {
-    show: {}
+    imgSrc: "",
+    title: "",
+    releaseDate: "",
+    userScore: "",
+    genres: []
   };
 
   componentDidMount() {
@@ -21,27 +25,55 @@ class MoviesDetailsPage extends Component {
     api
       .searcShowById(match.params.moviId)
       .then(res => {
-        this.setState({ show: res.data });
+        this.setState({
+          imgSrc: res.data.poster_path,
+          releaseDate: res.data.release_date,
+          userScore: res.data.vote_average,
+          genres: [...res.data.genres],
+          title: res.data.title,
+          overview: res.data.overview
+        });
+        return res;
       })
+
       .catch(err => {
-        console.log(err);
+        throw new Error(err);
       });
   }
 
   render() {
-    const { show } = this.state;
+    const {
+      imgSrc,
+      releaseDate,
+      title,
+      userScore,
+      genres,
+      overview
+    } = this.state;
 
     return (
       <>
-        <div className={style.wrapper}>
-          <h2 className={style.title}>
-            {`${show.title} (${show.release_date})`}{" "}
-          </h2>
-          <img
-            src={`https://image.tmdb.org/t/p/w400${show.poster_path}`}
-            className={style.showImage}
-            alt="film prewiev "
-          />
+        <div className={style.container}>
+          <div className={style.wrapper}>
+            <img
+              src={`https://image.tmdb.org/t/p/w400${imgSrc}`}
+              className={style.showImage}
+              alt="film prewiev "
+            />
+            <div className={style.innerWrapper}>
+              <h2 className={style.title}>{`${title} (${releaseDate})`} </h2>
+              <span className={style.text}>
+                {userScore ? `User Score: ${userScore * 10}%` : null}
+              </span>
+              <span className={style.boldText}>Overview</span>
+              <span className={style.text}>{overview}</span>
+              <span className={style.boldText}>Genres</span>
+
+              <span className={style.text}>
+                {genres.map(el => `${el.name} `)}
+              </span>
+            </div>
+          </div>
         </div>
       </>
     );
